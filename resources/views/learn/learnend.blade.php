5fc8f2af6d5f9ab8035ca2ea1e87fb32e40b9394
Ubuntu <ubuntu@ip-172-31-49-209.ec2.internal>
2022-04-30 09:48:56 +0000

    first commit

--- resources/views/learn/learnend.blade.php
+++ resources/views/learn/learnend.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('学習モード') }}
+      {{ __('リカモス | 学習モード') }}
     </h2>
   </x-slot>
 
