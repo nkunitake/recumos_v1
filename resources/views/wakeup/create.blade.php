7bb09b3fb940ba002a8dc114a9e4f80081ce2b1b
Ubuntu <ubuntu@ip-172-31-49-209.ec2.internal>
2022-04-30 10:18:27 +0000

    wakeup update

--- resources/views/wakeup/create.blade.php
+++ resources/views/wakeup/create.blade.php
@@ -1,25 +1,25 @@
 <x-app-layout>
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('早起きモード') }}
+      {{ __('リカモス | 早起きモード') }}
     </h2>
   </x-slot>
 
   <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:w-8/12 md:w-1/2 lg:w-5/12">
-      <!--<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">-->
-        <!--<div class="p-6 bg-white border-b border-gray-200">-->
-          <form action="{{ route('wakeup.store') }}" method="POST">
-            @csrf
-                        <input type="hidden" name="wakeup_at" value="{{ \Carbon\Carbon::now() }}">
-                        <input type="hidden" name="feeling" value="5">
-                        <input type="hidden" name="comment" value="ホゲホゲ"> 
-            <button type="submit" name="manual" value="0" class="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
+    <div class="flex flex-col text-center w-full mb-8">
+        <h3 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">今日も早起き頑張りましたね！</h3>
+          <p>{{ \Carbon\Carbon::now()->month }}月{{ \Carbon\Carbon::now()->day }}日{{ \Carbon\Carbon::now()->hour }}時{{ \Carbon\Carbon::now()->hour }}分に起きました。</p>
+    </div>
+    <div class="flex justify-center max-w-7xl mx-auto sm:w-8/12 md:w-1/2 lg:w-5/12">
+        <form action="{{ route('wakeup.store') }}" method="POST">
+          @csrf
+              <input type="hidden" name="wakeup_at" value="{{ \Carbon\Carbon::now() }}">
+              <input type="hidden" name="feeling" value="5">
+              <input type="hidden" name="comment" value="ホゲホゲ"> 
+            <button type="submit" name="manual" value="0" class="bg-gray-600 text-white px-32 py-3 rounded-md text-1xl font-medium hover:bg-gray-700 transition duration-300">
               早起きを記録
             </button>
-          </form>
-        <!--</div>-->
-      <!--</div>-->
+        </form>
     </div>
   </div>
 </x-app-layout>
